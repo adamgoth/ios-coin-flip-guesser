a5b32da6d702c7b64da63bf1cd0512c704fcbff7
Adam Goth <adamgoth@gmail.com>
2016-07-08 19:04:44 -0600

    Adds basic flip functionality

--- coin-flip-guesser/ViewController.swift
+++ coin-flip-guesser/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var headsOrTails: UILabel!
+    
+    @IBOutlet weak var winOrLose: UILabel!
+    
+    var coin: [String] = ["Heads", "Tails"]
+    var userSelection: String = "Heads"
+    var flipResult: String!
+    var rand: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func flipPressed(sender: AnyObject) {
+        rand = Int(arc4random_uniform(2))
+        flipResult = coin[rand]
+        headsOrTails.text = flipResult
+        if flipResult == userSelection {
+            winOrLose.text = "You win!"
+        } else {
+            winOrLose.text = "You lose!"
+        }
     }
-
-
+    
+    @IBAction func indexChanged(sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+            case 0:
+                userSelection = "Heads"
+            case 1:
+                userSelection = "Tails"
+            default:
+                break
+        }
+    }
 }
 
